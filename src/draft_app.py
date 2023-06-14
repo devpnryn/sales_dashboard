7ec667e29f3f1ddc010547d2d783848fd1707834
devpnryn <devpnarayana@gmail.com>
2023-06-14 23:54:57 +0100

    new UI changes

--- src/draft_app.py
+++ src/draft_app.py
@@ -3,106 +3,139 @@ import numpy as np  # pip install numpy
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 from streamlit_option_menu import option_menu
+import base64
+
+
+# local python imports
+# import image_loader as iml
+
 
 st.set_page_config(page_title='Retail Forecast',
                    page_icon=':bar_chart:', layout='centered')
 st.markdown('## Retail Forecast')
 
+# --- VARIABLES ---
+# css_file = './styles/style.css'
+# --- UTILITIES ---
+
+#  To set background image
+
+
+@st.cache_data
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp{
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+
+#  To read excel file
+@st.cache_data
+def get_data(filepath) -> pd.DataFrame:
+    df = pd.read_excel(filepath)
+    return df
+
+# TODO: set the background image without effecting the darkmode
+# set_png_as_page_bg('assets/Jan-Business_report_1.jpg')
+# set_png_as_page_bg('assets/analytics-svgrepo-com.svg')
+
+
 # --- SIDE BAR ---
 st.sidebar.header("Select Products")
 uploaded_file = st.sidebar.file_uploader("Choose a File", type='xlsx')
 
-# --- Navigation Menu ---
-selected = option_menu(
-    menu_title=None,
-    options=["Data Details", "Analytics"],
-    icons=["pencil-fill", "bar-chart-fill"],
-    orientation="horizontal")
 
-df_selection = None
+if uploaded_file is None:
+    st.write('upload excel file to see the insights')
 
-
-def read_file(file):
-    df = pd.DataFrame()
-    if file is not None:
-        df = pd.read_excel(file)
-        st.markdown('### Uploaded file contents')
-        st.write(f'{file.name} is uploaded!')
-        st.dataframe(df)
-    else:
-        st.warning("you need to upload excel file.")
-    return df
-
-
-df = read_file(uploaded_file)
-# --- new approach ---
 if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    selected_product = st.selectbox('Which product category are you interested in?',
-                                    options=df["productName"].unique()
-                                    )
-    output_columns = ['quantity']
-    if selected_product is not None:
-        df_query = df.query("productName== @selected_product")
-        product_quantity = df_query[output_columns].sum(numeric_only=True)
-        st.dataframe(product_quantity)
+    df = get_data(uploaded_file)
 
-    #  Sales trendline of the product
-    product_df = df[df['productName'] == selected_product]
-    rolling_mean = product_df['quantity'].rolling(window=7).mean()
-    fig = px.line(product_df,
-                  x='transactionDate',
-                  y=rolling_mean,
-                  labels={'transactionDate': 'Transaction Date',
-                          'rolling_mean': 'Quantity'},
-                  title=f'{selected_product} Sales Trend')
-    fig.update_xaxes(dtick='M1', tickformat='%b-%y')
-    st.plotly_chart(fig, use_container_width=True)
+    # -- store the dataframe in session for the future workflow..
+    if 'df_excel' not in st.session_state:
+        st.session_state['df_excel'] = df
 
-if not df.empty:
-    madeby_ = st.sidebar.multiselect(
-        "Select Manufacturer:",
-        options=df["manufacturer"].unique(),
-        default=df["manufacturer"].unique()[0]
-    )
+    #  display dataframe to the screen
+    # st.session_state['df_excel']
 
-    prod_ = st.sidebar.multiselect(
-        "Select Component:",
-        options=df.query("manufacturer==@madeby_")["productName"].unique(),
-        default=df.query("manufacturer==@madeby_")["productName"].unique()
-    )
-    if 'read_data' not in st.session_state:
-        st.session_state["read_data"] = df.query(
-            "productName==@prod_ & manufacturer== @madeby_")
+    # --- Navigation Menu ---
+    selected = option_menu(
+        menu_title=None,
+        options=["Key Insights", "More Analytics"],
+        icons=["pencil-fill", "bar-chart-fill"],
+        orientation="horizontal")
+    # -- store the dataframe in session for the future workflow..
+    # if 'current_tab' not in st.session_state:
+    #     st.session_state['current_tab'] = selected
+    st.session_state['current_tab'] = selected
 
-if 'read_data' not in st.session_state:
-    st.write('upload excel to see it here!')
-elif 'read_data' in st.session_state:
-    df_selection = st.session_state['read_data']
 
-if selected == "Data Details":
-    st.markdown('### Selected fields')
-    if df_selection is not None:
-        df_selection.index = np.arange(1, len(df_selection) + 1)
-        st.dataframe(df_selection)
-elif selected == "Analytics":
-    # sales by category
-    if df_selection is not None:
-        sales_by_product_type = (
-            df_selection.groupby("productName").sum(
-                numeric_only=True)[["quantity"]]
-        )
-        fig_product_sales = px.bar(
-            sales_by_product_type,
-            x="quantity",
-            y=sales_by_product_type.index,
-            title="<b> Sales by Product</b>",
-            color_discrete_sequence=["#008388"] * len(sales_by_product_type),
-            template="plotly_white"
-        )
-        st.plotly_chart(fig_product_sales)
-        #         sales trendline of the product
+if uploaded_file:
+
+    # --- UI under Key insights Tab --- #
+    if st.session_state['current_tab'] == "Key Insights":
+        st.markdown('### insights')
+    # --- Three widgets: 1. Total sales 2. Unique Prodcuts 3.YoY Sales Trend --- #
+
+    # creating a single-element container
+        widgets = st.empty()
+        with widgets.container():
+
+            # create three columns
+            kpi1, kpi2, kpi3 = st.columns(3)
+
+            # fill in those three columns with respective metrics or KPIs
+            kpi1.metric(
+                label="Total Sales ⏳",
+                value=25000,
+                delta=30,
+            )
+
+            kpi2.metric(
+                label="Products 🎳",
+                value=328,
+                delta=-10 + 32,
+            )
+
+            kpi3.metric(
+                label="Best Year 🎉",
+                value=2020,
+                delta=49,
+            )
+            # LINK TO THE CSS FILE
+            with open("src/styles/style.css")as f:
+                st.markdown(f"<style> {f.read()}</style>",
+                            unsafe_allow_html=True)
+
+            st.markdown("<hr/>", unsafe_allow_html=True)
+
+            #  display dataframe to the screen
+            st.session_state['df_excel']
+
+            #  --- some trend charts --- #
+            # create two columns for charts
+            # --- 1. YoY sales growth trend
+            # ----2. Fast selling products
+
+    # --- UI under Analytics Tab --- #
+    elif st.session_state['current_tab'] == "More Analytics":
+        st.markdown('### analytics')
+    #  --- Filters with options --- #
+
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
@@ -112,116 +145,4 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-st.set_page_config(page_title='Retail Forecast',
-                   page_icon=':bar_chart:', layout='centered')
-st.markdown('## Sales Forecast')
-
-# --- SIDE BAR ---
-st.sidebar.header("Select Products")
-uploaded_file = st.sidebar.file_uploader("Choose a File", type='xlsx')
-
-# --- Navigation Menu ---
-selected = option_menu(
-    menu_title=None,
-    options=["Data Details", "Analytics"],
-    icons=["pencil-fill", "bar-chart-fill"],
-    orientation="horizontal")
-
-df_selection = None
-
-
-def read_file(file):
-    df = pd.DataFrame()
-    if file is not None:
-        df = pd.read_excel(file)
-        st.markdown('### Uploaded file contents')
-        st.write(f'{file.name} is uploaded!')
-        st.dataframe(df)
-    else:
-        st.warning("you need to upload excel file.")
-    return df
-
-
-df = read_file(uploaded_file)
-# --- new approach ---
-if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    selected_product = st.selectbox('Which product category are you interested in?',
-                                    options=df["productName"].unique()
-                                    )
-    output_columns = ['quantity']
-    if selected_product is not None:
-        df_query = df.query("productName== @selected_product")
-        product_quantity = df_query[output_columns].sum(numeric_only=True)
-        st.dataframe(product_quantity)
-
-    #  Sales trendline of the product
-    product_df = df[df['productName'] == selected_product]
-    rolling_mean = product_df['quantity'].rolling(window=7).mean()
-    fig = px.line(product_df,
-                  x='transactionDate',
-                  y=rolling_mean,
-                  labels={'transactionDate': 'Transaction Date',
-                          'rolling_mean': 'Quantity'},
-                  title=f'{selected_product} Sales Trend')
-    fig.update_xaxes(dtick='M1', tickformat='%b-%y')
-    st.plotly_chart(fig, use_container_width=True)
-
-if not df.empty:
-    madeby_ = st.sidebar.multiselect(
-        "Select Manufacturer:",
-        options=df["manufacturer"].unique(),
-        default=df["manufacturer"].unique()[0]
-    )
-
-    prod_ = st.sidebar.multiselect(
-        "Select Component:",
-        options=df.query("manufacturer==@madeby_")["productName"].unique(),
-        default=df.query("manufacturer==@madeby_")["productName"].unique()
-    )
-    if 'read_data' not in st.session_state:
-        st.session_state["read_data"] = df.query(
-            "productName==@prod_ & manufacturer== @madeby_")
-
-if 'read_data' not in st.session_state:
-    st.write('upload excel to see it here!')
-elif 'read_data' in st.session_state:
-    df_selection = st.session_state['read_data']
-
-if selected == "Data Details":
-    st.markdown('### Selected fields')
-    if df_selection is not None:
-        df_selection.index = np.arange(1, len(df_selection) + 1)
-        st.dataframe(df_selection)
-elif selected == "Analytics":
-    # sales by category
-    if df_selection is not None:
-        sales_by_product_type = (
-            df_selection.groupby("productName").sum(
-                numeric_only=True)[["quantity"]]
-        )
-        fig_product_sales = px.bar(
-            sales_by_product_type,
-            x="quantity",
-            y=sales_by_product_type.index,
-            title="<b> Sales by Product</b>",
-            color_discrete_sequence=["#008388"] * len(sales_by_product_type),
-            template="plotly_white"
-        )
-        st.plotly_chart(fig_product_sales)
-        #         sales trendline of the product
-
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-
 st.markdown(hide_st_style, unsafe_allow_html=True)
