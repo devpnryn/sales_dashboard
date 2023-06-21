d45c8ef50a2603cbc9f1fa1f558b83ec098007bc
devpnryn <devpnarayana@gmail.com>
2023-06-21 19:53:08 +0100

    updated UI for draft_app and training model

---
+++ src/dataprocessor.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+
+# reads the data from excel sheets
+# creates df with proper index
+
+def get_raw_dataframe(filename) -> pd.DataFrame:
+    df = pd.concat(pd.read_excel(filename, sheet_name=None), ignore_index=True)
+    df = df.set_index('Sale Date')
+    df.index = pd.to_datetime(df.index)
+    return df
+
+# filters the given df by product name
+# can handle filter by single or multiple products
+
+
+def filter_by_product(df, product) -> pd.DataFrame:
+    if isinstance(product, str):
+        product = [product]
+    df = df[df['Product'].isin(product)]
+    return df
+
+
+# filters the given df by brand name
+def filter_by_brand(df, brand) -> pd.DataFrame:
+    df = df[df.Product == brand]
+    return df
+
+# Creates extra features to the raw df
+
+
+def create_features(df) -> pd.DataFrame:
+    """
+    Create time series features based on time series index 
+    """
+    df = df.copy()
+    df['dayofweek'] = df.index.day_of_week
+    df['quarter'] = df.index.quarter
+    df['month'] = df.index.month
+    df['dayofyear'] = df.index.day_of_year
+    df['year'] = df.index.year
+    return df
--- src/draft_app.py
+++ src/draft_app.py
@@ -3,19 +3,26 @@ import numpy as np  # pip install numpy
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 from streamlit_option_menu import option_menu
-import base64
+import plotly.figure_factory as ff
 
+import base64
+# local imports
+import dataprocessor
 
 # local python imports
 # import image_loader as iml
 
-
+# setting favicon and browser tab title
 st.set_page_config(page_title='Retail Forecast',
                    page_icon=':bar_chart:', layout='centered')
-st.markdown('## Retail Forecast')
 
+# setting up Page Title
+st.markdown('## Retail Forecast')
 # --- VARIABLES ---
-# css_file = './styles/style.css'
+
+nav_menu = st.container()
+# st.session_state['current_tab'] = 'Key Insights'
+st.session_state["selected_products"] = None
 # --- UTILITIES ---
 
 #  To set background image
@@ -53,88 +60,143 @@ def get_data(filepath) -> pd.DataFrame:
 # set_png_as_page_bg('assets/Jan-Business_report_1.jpg')
 # set_png_as_page_bg('assets/analytics-svgrepo-com.svg')
 
+# To draw scatter plots for comparison
+
+
+def draw_scatter_plot(df, product_column, x_column, y_column):
+    fig = px.scatter(df, x=df.index.quarter, y=y_column, color=product_column,
+                     labels={x_column: 'X-axis', y_column: 'Y-axis'},
+                     title='Scatter Plot by Product')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# To draw trend line chart for multiple products
+def draw_trend_line_chart(df, product_column, date_column, quantity_column):
+    fig = px.line(df, x=df.index.year, y=df[quantity_column].rolling(200).mean(), color=product_column,
+                  labels={date_column: 'Sale Date',
+                          quantity_column: 'Quantity'},
+                  title='Quantity Trend by Product')
+
+    fig.update_xaxes(dtick='M1', tickformat='%b-%y')
+    st.plotly_chart(fig, use_container_width=True)
+
 
 # --- SIDE BAR ---
-st.sidebar.header("Select Products")
-uploaded_file = st.sidebar.file_uploader("Choose a File", type='xlsx')
+st.sidebar.header("Welcome Team")
+
+uploaded_file = st.sidebar.file_uploader(
+    "Choose a File", type='xlsx')
 
 
 if uploaded_file is None:
     st.write('upload excel file to see the insights')
 
 if uploaded_file:
-    df = get_data(uploaded_file)
+    # df = get_data(uploaded_file)
+    df = dataprocessor.get_raw_dataframe(uploaded_file)
 
     # -- store the dataframe in session for the future workflow..
     if 'df_excel' not in st.session_state:
         st.session_state['df_excel'] = df
 
-    #  display dataframe to the screen
-    # st.session_state['df_excel']
-
     # --- Navigation Menu ---
-    selected = option_menu(
-        menu_title=None,
-        options=["Key Insights", "More Analytics"],
-        icons=["pencil-fill", "bar-chart-fill"],
-        orientation="horizontal")
-    # -- store the dataframe in session for the future workflow..
-    # if 'current_tab' not in st.session_state:
-    #     st.session_state['current_tab'] = selected
-    st.session_state['current_tab'] = selected
+    with nav_menu:
+        selected = option_menu(
+            menu_title=None,
+            options=["Key Insights", "More Analytics"],
+            icons=["pencil-fill", "bar-chart-fill"],
+            orientation="horizontal")
+        # -- store the dataframe in session for the future workflow..
+        # if st.session_state['current_tab'] is None:
+        st.session_state['current_tab'] = selected
 
+        # --- UI under Key insights Tab --- #
+        if st.session_state['current_tab'] == "Key Insights":
+            st.markdown('### Key Metrics')
+        # --- Three widgets: 1. Total sales 2. Unique Prodcuts 3.YoY Sales Trend --- #
 
-if uploaded_file:
-
-    # --- UI under Key insights Tab --- #
-    if st.session_state['current_tab'] == "Key Insights":
-        st.markdown('### insights')
-    # --- Three widgets: 1. Total sales 2. Unique Prodcuts 3.YoY Sales Trend --- #
-
-    # creating a single-element container
-        widgets = st.empty()
-        with widgets.container():
-
+        # creating a single-element container
             # create three columns
             kpi1, kpi2, kpi3 = st.columns(3)
 
-            # fill in those three columns with respective metrics or KPIs
+            # fill in those three columns with respective KPIs
+            # KPI-1: Total sales so far
+            total = df['Quantity'].sum()
             kpi1.metric(
                 label="Total Sales ⏳",
-                value=25000,
+                value=total,
                 delta=30,
             )
 
+            # KPI-2: No.of unique products
+            products = df['Product'].unique()
             kpi2.metric(
                 label="Products 🎳",
-                value=328,
+                value=products.size,
                 delta=-10 + 32,
             )
 
+            # KPI-3: Best year so far in terms of sales
+            grouped_df = df.groupby(df.index.year)['Quantity'].sum()
             kpi3.metric(
                 label="Best Year 🎉",
-                value=2020,
+                value=grouped_df.idxmax(),
                 delta=49,
             )
-            # LINK TO THE CSS FILE
+            # styling the kpis
             with open("src/styles/style.css")as f:
                 st.markdown(f"<style> {f.read()}</style>",
                             unsafe_allow_html=True)
 
             st.markdown("<hr/>", unsafe_allow_html=True)
+            #  --- some charts --- #
 
-            #  display dataframe to the screen
-            st.session_state['df_excel']
+            # products = df['Product'].unique()
 
-            #  --- some trend charts --- #
-            # create two columns for charts
-            # --- 1. YoY sales growth trend
+            # Selection box with product names
+            if st.session_state["selected_products"] is None:
+                options = st.multiselect(
+                    'YoY sales of following products', products, products[:1])
+
+                st.session_state["selected_products"] = options
+
+            df = dataprocessor.filter_by_product(
+                df, st.session_state["selected_products"])
+
+            # creating new feature(Year) on dataframe
+            df['year'] = df.index.year
+
+            # create two tabs for charts
+
+            tab1, tab2 = st.tabs(["Scatter plot", "YoY sales"])
+            st.session_state["insights_df"] = df
+
+            # --- Tab 1. Scatter plot to show the distribution
+            with tab1:
+                if st.session_state["insights_df"] is None:
+                    draw_scatter_plot(df, "Product", 'Sale Date', 'Quantity')
+                else:
+                    draw_scatter_plot(
+                        st.session_state["insights_df"], "Product", 'Sale Date', 'Quantity')
+
+            # --- Tab 2. Trend line to show the YoY growth
+            with tab2:
+                # TODO: this has to be rolling mean of each selected product with different color on same plot
+                # plot_sales_trend(df, options)
+                if st.session_state["insights_df"] is None:
+                    draw_trend_line_chart(
+                        df, 'Product', 'Sale Date', 'Quantity')
+                else:
+                    draw_trend_line_chart(
+                        st.session_state["insights_df"], 'Product', 'Sale Date', 'Quantity')
             # ----2. Fast selling products
+            st.subheader('Fast selling products')
 
-    # --- UI under Analytics Tab --- #
-    elif st.session_state['current_tab'] == "More Analytics":
-        st.markdown('### analytics')
-    #  --- Filters with options --- #
+        # --- UI under Analytics Tab --- #
+        elif st.session_state['current_tab'] == "More Analytics":
+            st.markdown('### analytics')
+        #  --- Filters with options --- #
 
 
 # ---- HIDE STREAMLIT STYLE ----
@@ -145,4 +207,4 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# st.markdown(hide_st_style, unsafe_allow_html=True)
