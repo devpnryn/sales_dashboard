d151dcb50ae3e6b9f9e442a57a1ffc585ca8ea53
devpnryn <devpnarayana@gmail.com>
2023-06-22 15:51:58 +0100

    plots are updated

--- src/draft_app.py
+++ src/draft_app.py
@@ -9,8 +9,6 @@ import base64
 # local imports
 import dataprocessor
 
-# local python imports
-# import image_loader as iml
 
 # setting favicon and browser tab title
 st.set_page_config(page_title='Retail Forecast',
@@ -21,7 +19,6 @@ st.markdown('## Retail Forecast')
 # --- VARIABLES ---
 
 nav_menu = st.container()
-# st.session_state['current_tab'] = 'Key Insights'
 st.session_state["selected_products"] = None
 # --- UTILITIES ---
 
@@ -33,6 +30,8 @@ def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
+
+# To set the background image for the app.
 
 
 def set_png_as_page_bg(png_file):
@@ -63,15 +62,48 @@ def get_data(filepath) -> pd.DataFrame:
 # To draw scatter plots for comparison
 
 
-def draw_scatter_plot(df, product_column, x_column, y_column):
-    fig = px.scatter(df, x=df.index.quarter, y=y_column, color=product_column,
-                     labels={x_column: 'X-axis', y_column: 'Y-axis'},
-                     title='Scatter Plot by Product')
+def draw_scatter_plot(df):
+    df = df.copy()
+    df['Date'] = pd.to_datetime(df['year'])
+    df['Year'] = df['Date'].dt.year
+    df['Quarter'] = df['Date'].dt.quarter
+    # st.write(df.groupby(['Year', 'Brand', 'Product']).agg(
+    #     {'Total Price': 'sum', 'Quantity': 'sum'}))
 
+    # Convert the 'Year' column to datetime and extract year and quarter
+    df['Year'] = pd.to_datetime(df['Year'], format='%Y')
+    df['Year'] = df['Year'].dt.year
+
+    # Group by Year, Brand, and Product and calculate the sum of Total Price and Quantity
+    df_grouped = df.groupby(['Year', 'Brand', 'Product']).agg(
+        {'Total Price': 'sum', 'Quantity': 'sum'}).reset_index()
+
+    # Pivot the DataFrame to have 'Year' as columns
+    pivot_df = df_grouped.pivot(
+        index=['Brand', 'Product'], columns='Year', values='Total Price').reset_index()
+
+    # Melt the pivoted DataFrame to long format
+    df_melted = pivot_df.melt(
+        id_vars=['Brand', 'Product'], var_name='Year', value_name='Total Price')
+
+    # Plotting the stacked bar chart using Plotly Express
+    fig = px.bar(df_melted, x='Year', y='Total Price',
+                 color='Brand', barmode='stack')
+
+    # Update the layout
+    fig.update_layout(
+        title='Total Sales by Brand and Product per Year',
+        xaxis_title='Year',
+        yaxis_title='Total Price'
+    )
+
+    # Display the chart using Streamlit
     st.plotly_chart(fig, use_container_width=True)
 
 
 # To draw trend line chart for multiple products
+
+
 def draw_trend_line_chart(df, product_column, date_column, quantity_column):
     fig = px.line(df, x=df.index.year, y=df[quantity_column].rolling(200).mean(), color=product_column,
                   labels={date_column: 'Sale Date',
@@ -114,7 +146,7 @@ if uploaded_file:
         # --- UI under Key insights Tab --- #
         if st.session_state['current_tab'] == "Key Insights":
             st.markdown('### Key Metrics')
-        # --- Three widgets: 1. Total sales 2. Unique Prodcuts 3.YoY Sales Trend --- #
+        # --- Three widgets: 1. Total sales 2. Unique Prodcuts 3.Best Year of Sales  --- #
 
         # creating a single-element container
             # create three columns
@@ -126,15 +158,15 @@ if uploaded_file:
             kpi1.metric(
                 label="Total Sales ⏳",
                 value=total,
-                delta=30,
+                delta=300,
             )
 
-            # KPI-2: No.of unique products
+            # KPI-2: No.of unique products sold
             products = df['Product'].unique()
             kpi2.metric(
                 label="Products 🎳",
                 value=products.size,
-                delta=-10 + 32,
+                delta=-10 + products.size,
             )
 
             # KPI-3: Best year so far in terms of sales
@@ -144,20 +176,19 @@ if uploaded_file:
                 value=grouped_df.idxmax(),
                 delta=49,
             )
+
             # styling the kpis
             with open("src/styles/style.css")as f:
                 st.markdown(f"<style> {f.read()}</style>",
                             unsafe_allow_html=True)
 
             st.markdown("<hr/>", unsafe_allow_html=True)
-            #  --- some charts --- #
 
-            # products = df['Product'].unique()
-
-            # Selection box with product names
+            # Selection box with product names, by default shows two products
+            st.subheader("Sales Trends of the products")
             if st.session_state["selected_products"] is None:
                 options = st.multiselect(
-                    'YoY sales of following products', products, products[:1])
+                    label='', options=products, default=products[:2])
 
                 st.session_state["selected_products"] = options
 
@@ -165,32 +196,37 @@ if uploaded_file:
                 df, st.session_state["selected_products"])
 
             # creating new feature(Year) on dataframe
-            df['year'] = df.index.year
+            # st.write(pd.to_datetime(df.index, format="%Y"))
+            df['year'] = pd.to_datetime(df.index, format="%Y")
 
             # create two tabs for charts
 
-            tab1, tab2 = st.tabs(["Scatter plot", "YoY sales"])
+            tab1, tab2 = st.tabs(
+                ["Sales volume by Price", "Sales by Products"])
             st.session_state["insights_df"] = df
 
             # --- Tab 1. Scatter plot to show the distribution
             with tab1:
+                # TODO: this has to draw scatter plot of different product sales in every year
+                # st.write(st.session_state["insights_df"])
                 if st.session_state["insights_df"] is None:
-                    draw_scatter_plot(df, "Product", 'Sale Date', 'Quantity')
+                    draw_scatter_plot(df)
                 else:
                     draw_scatter_plot(
-                        st.session_state["insights_df"], "Product", 'Sale Date', 'Quantity')
+                        st.session_state["insights_df"])
 
             # --- Tab 2. Trend line to show the YoY growth
             with tab2:
                 # TODO: this has to be rolling mean of each selected product with different color on same plot
-                # plot_sales_trend(df, options)
                 if st.session_state["insights_df"] is None:
                     draw_trend_line_chart(
                         df, 'Product', 'Sale Date', 'Quantity')
                 else:
                     draw_trend_line_chart(
                         st.session_state["insights_df"], 'Product', 'Sale Date', 'Quantity')
+
             # ----2. Fast selling products
+
             st.subheader('Fast selling products')
 
         # --- UI under Analytics Tab --- #
