47392926c2fcc07a9b5a3cfc3626521b25ee6276
devpnryn <devpnarayana@gmail.com>
2023-07-02 00:07:53 +0100

    removed clutter

--- src/login.py
+++
@@ -1,9 +0,0 @@
-# product_dict = {
-#     "keyboard":
-#     {
-#         "details":{"brand":"Zebronics", "Price":[]}
-#         "barnds": ["Zebronics", "HP", "Dell", "Intex", "Amkette", "Amazon Basics", "Quantum", "Logitech", "Apple", "Lenovo", "iBall", "TVS"],
-#         "prices": [500, 1500, 2500],
-#     },
-#     "mouse": ""
-# }
